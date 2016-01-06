34e6b92d7ffae136eeac19e313d52401d53aef30
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 15:40:33 -0800

    Adds math functions to calculator.

--- calculator.py
+++ calculator.py
@@ -15,16 +15,33 @@ def calculator():
         math = raw_input(">")
         #     tokenize input
         tokens = math.split(' ')
-        operator = tokens.pop()
+        operator = tokens.pop(0)
 
         #     if the first token is 'q', quit
         if operator == "q":
             break
         #     otherwise decide which math function to call based on the tokens we read
         elif operator == "+":
-            answer = add(tokens.pop(), tokens.pop())
-            for 
+            answer = add(tokens[0], tokens[1])
+        elif operator == "-":
+            answer = subtract(tokens[0], tokens[1])
+        elif operator == "*":
+            answer = multiply(tokens[0], tokens[1])
+        elif operator == "/":
+            answer = divide(tokens[0], tokens[1])
+        elif operator == "%":
+            answer = mod(tokens[0], tokens[1])
+        elif operator == "cube":
+            answer = cube(tokens[0], tokens[1])
+        elif operator == "pow":
+            answer = power(tokens[0], tokens[1])
+        elif operator == "square":
+            answer = square(tokens[0], tokens[1])
+        else:
+            print "I don't understand."
 
+        if answer:
+            print answer
 
 
 calculator()
