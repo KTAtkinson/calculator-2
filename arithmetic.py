f78ecb75ab63400ebd2da3bc9ba6462fbd26d72d
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 13:00:25 -0800

    Started calculator.

--- arithmetic.py
+++ arithmetic.py
@@ -1,34 +1,31 @@
 def add(num1, num2):
-    return num1 + num2
-
+    result = num1 + num2
+    return result
 
 def subtract(num1, num2):
-    return num1 - num2
-
+    result = num1 - num2
+    return result
 
 def multiply(num1, num2):
-    return num1 * num2
-
+    result = num1 * num2
+    return result
 
 def divide(num1, num2):
-    # Need to turn at least argument to float for division to
-    # not be integer division
-    return float(num1) / float(num2) 
-
+    result = float(num1) / num2
+    return result
 
 def square(num1):
-    # Needs only one argument
+    """return the square of provided number"""
     return num1 * num1
 
-
 def cube(num1):
-    # Needs only one argument
+    """return the cube of the number"""
     return num1 * num1 * num1
 
-
 def power(num1, num2):
-    return num1 ** num2  # ** = exponent operator
-
+    """return result of raising num1 to the power of num2"""
+    return num1 ** num2
 
 def mod(num1, num2):
+    """returns the remainder when num1 is divided by num2"""
     return num1 % num2
--- calculator.py
+++ calculator.py
@@ -7,5 +7,24 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
+def calculator():
+    answer = None
+    # repeat forever:
+    while True:
+        #     read input
+        math = raw_input(">")
+        #     tokenize input
+        tokens = math.split(' ')
+        operator = tokens.pop()
 
-# Your code goes here
+        #     if the first token is 'q', quit
+        if operator == "q":
+            break
+        #     otherwise decide which math function to call based on the tokens we read
+        elif operator == "+":
+            answer = add(tokens.pop(), tokens.pop())
+            for 
+
+
+
+calculator()
